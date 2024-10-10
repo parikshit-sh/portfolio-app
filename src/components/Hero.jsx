00721e548c270a3f0f4e1d5fdb89dffbed4062b7
Parikshit Sharma <parikshit.p450@gmail.com>
2024-10-10 07:33:20 +0530

    add dvh values

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,18 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import "../index.css";
 
 const Hero = () => {
-  const heroRef = useRef(null);
   const { scrollY } = useScroll();
   const opacity = useTransform(scrollY, [0, 400], [1, 0]);
 
   useEffect(() => {
     const setVh = () => {
-      let vh = window.innerHeight * 0.01;
+      const vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
     };
 
     setVh();
-
     window.addEventListener('resize', setVh);
     window.addEventListener('orientationchange', setVh);
 
@@ -27,19 +25,18 @@ const Hero = () => {
   return (
     <motion.div 
       className="hero-container"
-      ref={heroRef} 
       style={{ opacity }}
     >
       <div className="hero-content">
-        <h1 className="hero-head" style={{fontFamily: 'PPEditorial'}}>
+        <h1 className="hero-head">
           <span className="cursive">P</span>ARIKSHIT{" "}
           <span className="cursive">S</span>HARMA
         </h1>
-        <h1 className="hero-head" style={{fontFamily: 'PPEditorial'}}>
+        <h1 className="hero-head">
           FRONT END{" "}
           <span className="developer">DEVELOPER</span>
         </h1>
-        <h1 className="icon uppercase" style={{fontFamily: 'PPEditorial'}}>
+        <h1 className="icon uppercase">
           Folio
           <span className="developer">&copy;</span>2024
           <span className="developer">⚗️✨</span>
